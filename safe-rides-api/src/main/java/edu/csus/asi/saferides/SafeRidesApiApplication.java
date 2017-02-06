ff71e9c8b6cb8236c6ba102a385c144c35096d01
Zeeshan <shan916@icloud.com>
2017-02-05 16:26:27 -0800

    Added initial API project

---
+++ safe-rides-api/src/main/java/edu/csus/asi/saferides/SafeRidesApiApplication.java
@@ -0,0 +1,12 @@
+package edu.csus.asi.saferides;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafeRidesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafeRidesApiApplication.class, args);
+	}
+}
