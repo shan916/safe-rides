f4adf1fdd362aee89ce53ec62d3d0d05ab7c5787
Edward Ozeruga <ed@eoz.io>
2017-02-05 19:41:43 -0800

    Move files into safe-rides-web subfolder

--- safe-rides-web/test/karma.conf.js
+++ safe-rides-web/test/karma.conf.js
@@ -30,6 +30,8 @@ module.exports = function(config) {
       'bower_components/angular-sanitize/angular-sanitize.js',
       'bower_components/angular-touch/angular-touch.js',
       'bower_components/ngmap/build/scripts/ng-map.js',
+      'bower_components/moment/moment.js',
+      'bower_components/eonasdan-bootstrap-datetimepicker/build/js/bootstrap-datetimepicker.min.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       'app/scripts/**/*.js',
