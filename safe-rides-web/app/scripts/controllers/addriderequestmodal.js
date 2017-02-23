ef39760ce18aa00c60eed1f546f287454f8d530b
bryceLH <blh22buck@gmail.com>
2017-02-22 20:01:26 -0800

    up to date with dev as of 2-22-17

--- safe-rides-web/app/scripts/controllers/addriderequestmodal.js
+++ safe-rides-web/app/scripts/controllers/addriderequestmodal.js
@@ -10,22 +10,23 @@
 angular.module('safeRidesWebApp')
   .controller('AddriderequestmodalCtrl', function ($uibModalInstance) {
     var vm = this;
-    vm.riderequest = [
-      {csusid: undefined},
-      {phone: undefined},
-      {fname: undefined},
-      {lname: undefined},
-      {pickupline1: undefined},
-      {pickupline2: undefined},
-      {pickupcity: undefined},
-      {pickupzip: undefined},
-      {dropOffline1: undefined},
-      {dropOffline2: undefined},
-      {dropOffcity: undefined},
-      {dropOffzip: undefined},
-      {peopleCount: undefined},
-      {msg: undefined}
-    ];
+    vm.riderequest = {
+      csusid: undefined,
+      phone: undefined,
+      firstname: undefined,
+      lastname: undefined,
+      pickupLine1: undefined,
+      pickupLine2: undefined,
+      pickupCity: undefined,
+      pickupZip: undefined,
+      dropOffLine1: undefined,
+      dropOffLine2: undefined,
+      dropOffCity: undefined,
+      dropOffZip: undefined,
+      peopleCount: undefined,
+      msg: undefined
+    };
+
     vm.cancel = function(){
         $uibModalInstance.dismiss('cancel');
     };
