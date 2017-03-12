551df021b8632c07695556384a67ae7cfd182c28
Edward Ozeruga <ed@eoz.io>
2017-03-11 17:09:50 -0800

    Fix undefined rider name and boldificate name

--- safe-rides-web/app/scripts/controllers/confirmcancelrequestmodal.js
+++ safe-rides-web/app/scripts/controllers/confirmcancelrequestmodal.js
@@ -8,9 +8,9 @@
 * Controller of the safeRidesWebApp
 */
 angular.module('safeRidesWebApp')
-.controller('ConfirmCancelRequestModalCtrl', function($uibModalInstance, request) {
+.controller('ConfirmCancelRequestModalCtrl', function($uibModalInstance, request, $sanitize) {
     var vm = this;
-    vm.body = 'You selected to cancel ' + request.name + '\'s ride request. Are you sure you want to continue? The request will be remove from the queue and ' + request.name + ' will be notified that their request is canceled.';
+    vm.body = 'You selected to cancel <strong>' + $sanitize(request.requestorFirstName) + '\'s</strong> ride request. Are you sure you want to continue? The request will be remove from the queue and <strong>' + $sanitize(request.requestorFirstName) + '</strong> will be notified that their request is canceled.';
     vm.danger = true;
 
     vm.cancel = function() {
