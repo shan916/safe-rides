26356b74d1a198c09ae7e0bdc5d0cff8cb3d72c6
nitronarcosis <nitro.narcosis.biker@gmail.com>
2017-03-14 00:31:34 -0700

    Create serviceworker
    necessary for progressive web apps

--- safe-rides-web/app/scripts/app.js
+++ safe-rides-web/app/scripts/app.js
@@ -8,6 +8,18 @@
  *
  * Main module of the application.
  */
+ if ('serviceWorker' in navigator) {
+   window.addEventListener('load', function() {
+     navigator.serviceWorker.register('/sw.js').then(function(registration) {
+       // Registration was successful
+       console.log('ServiceWorker registration successful with scope: ', registration.scope);
+     }).catch(function(err) {
+       // registration failed :(
+       console.log('ServiceWorker registration failed: ', err);
+     });
+   });
+ }
+
 angular
   .module('safeRidesWebApp', [
     'ngAnimate',
