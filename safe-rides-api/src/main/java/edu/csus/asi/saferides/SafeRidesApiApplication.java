8fa9e6a0e1ee1aaef4e2fbae72e994f766dd27e3
Edward <ozerugae@users.noreply.github.com>
2017-03-21 17:44:53 -0700

    Feature/swagger documentation (#39)
    * Add swagger documentation
    
    * Add some documentation to driver controller. Rearrange methods for readability
    
    * Add some documentation to rides controller.
    
    * remove some unnecessary dependencies

--- safe-rides-api/src/main/java/edu/csus/asi/saferides/SafeRidesApiApplication.java
+++ safe-rides-api/src/main/java/edu/csus/asi/saferides/SafeRidesApiApplication.java
@@ -11,8 +11,14 @@ import edu.csus.asi.saferides.model.RideRequestStatus;
 import edu.csus.asi.saferides.model.Vehicle;
 import edu.csus.asi.saferides.repository.DriverRepository;
 import edu.csus.asi.saferides.repository.RideRequestRepository;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class SafeRidesApiApplication {
 
 	public static void main(String[] args) {
@@ -85,6 +91,16 @@ public class SafeRidesApiApplication {
 			rideRequestRepository.save(rideRequest6);
 			rideRequestRepository.save(rideRequest7);
 		};
+	}
+
+	@Bean
+	public Docket swaggerSettings() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.any())
+				.paths(PathSelectors.any())
+				.build()
+				.pathMapping("/");
 	}
 
 }
--- safe-rides-api/src/main/java/edu/csus/asi/saferides/service/RideRequestController.java
+++ safe-rides-api/src/main/java/edu/csus/asi/saferides/service/RideRequestController.java
@@ -1,12 +1,16 @@
 package edu.csus.asi.saferides.service;
 
 import edu.csus.asi.saferides.model.RideRequest;
+import edu.csus.asi.saferides.model.RideRequestStatus;
 import edu.csus.asi.saferides.repository.RideRequestRepository;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-import edu.csus.asi.saferides.model.RideRequestStatus;
+
 import java.net.URI;
 import java.util.Date;
 
@@ -20,98 +24,102 @@ import java.util.Date;
 @RequestMapping("/rides")
 public class RideRequestController {
 
-	// this creates a singleton for RideRequestRepository
-	@Autowired
-	private RideRequestRepository rideRequestRepository;
-	
-	/*
-	 * GET "/rideRequests"
-	 * GET "/rideRequests?status=true"
-	 * GET "/rideRequests?status=false"
-	 * 
-	 * @return list rideRequests based on query param
-	 * */
-	@RequestMapping(method = RequestMethod.GET)
-	public Iterable<RideRequest> retrieveAll(@RequestParam(value = "status", required = false) RideRequestStatus status) {
-		return rideRequestRepository.findAll();
-	}
-	
-	/*
-	 * GET "/rideRequests/{id} 
-	 * 
-	 * @param id - the id of the rideRequest to find
-	 * 
-	 * @return rideRequest with specified id else not found
-	 * */
-	@RequestMapping(method = RequestMethod.GET, value="/{id}")
-	public ResponseEntity<?> retrieve(@PathVariable Long id) {
-		RideRequest result = rideRequestRepository.findOne(id);
-		
-		if (result == null) {
-			return ResponseEntity.notFound().build();
-		} else {
-			return ResponseEntity.ok(result);
-		}
-	}
-	
-	/*
-	 * POST "/rideRequests" 
-	 * 
-	 * Creates the given rideRequest
-	 * 
-	 * @param rideRequest - the rideRequest to be created in the database
-	 * 
-	 * @return HTTP response containing saved entity with status of "created" 
-	 *  	   and the location header set to location of the entity
-	 * */
-	@RequestMapping(method = RequestMethod.POST)
-	public ResponseEntity<?> save(@RequestBody RideRequest rideRequest) {
-		rideRequest.setDate(new Date());	// default to current datetime
-		rideRequest.setStatus(RideRequestStatus.UNASSIGNED);	// default to unassigned status
+    // this creates a singleton for RideRequestRepository
+    @Autowired
+    private RideRequestRepository rideRequestRepository;
 
-		RideRequest result = rideRequestRepository.save(rideRequest);
-		
-		// create URI of where the rideRequest was created
-		URI location = ServletUriComponentsBuilder
-				.fromCurrentRequest().path("/{id}")
-				.buildAndExpand(result.getId()).toUri();
-				
-		return ResponseEntity.created(location).body(result);
-	}
-	
-	/*
-	 * PUT "/rideRequests/{id}" 
-	 * 
-	 * Updates the given rideRequest
-	 * 
-	 * @param rideRequest - the rideRequest to be updated in the database
-	 * 
-	 * @return HTTP response containing saved entity with status of "ok"
-	 * */
-	@RequestMapping(method = RequestMethod.PUT, value = "/{id}")
-	public ResponseEntity<?> save(@PathVariable Long id, @RequestBody RideRequest rideRequest) {
-		RideRequest result = rideRequestRepository.save(rideRequest);
-				
-		return ResponseEntity.ok(result);
-	}
+    /*
+     * GET /rides
+     */
+    @RequestMapping(method = RequestMethod.GET)
+    @ApiOperation(value = "retrieveAll", nickname = "retrieveAll", notes = "Returns a list of rides...")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Success", response = RideRequest.class, responseContainer = "List"),
+            @ApiResponse(code = 401, message = "Unauthorized"),
+            @ApiResponse(code = 403, message = "Forbidden"),
+            @ApiResponse(code = 500, message = "Failure")})
+    public Iterable<RideRequest> retrieveAll(@RequestParam(value = "status", required = false) RideRequestStatus status) {
+        return rideRequestRepository.findAll();
+    }
+
+    /*
+     * GET /rides/{id}
+     */
+    @RequestMapping(method = RequestMethod.GET, value = "/{id}")
+    @ApiOperation(value = "retrieve", nickname = "retrieve", notes = "Returns a ride with the specified id")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Success", response = ResponseEntity.class),
+            @ApiResponse(code = 401, message = "Unauthorized"),
+            @ApiResponse(code = 403, message = "Forbidden"),
+            @ApiResponse(code = 500, message = "Failure")})
+    public ResponseEntity<?> retrieve(@PathVariable Long id) {
+        RideRequest result = rideRequestRepository.findOne(id);
+
+        if (result == null) {
+            return ResponseEntity.notFound().build();
+        } else {
+            return ResponseEntity.ok(result);
+        }
+    }
+
+    /*
+     * POST /rides
+     */
+    @RequestMapping(method = RequestMethod.POST)
+    @ApiOperation(value = "save", nickname = "save", notes = "Creates a given ride request")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Success", response = ResponseEntity.class),
+            @ApiResponse(code = 401, message = "Unauthorized"),
+            @ApiResponse(code = 403, message = "Forbidden"),
+            @ApiResponse(code = 500, message = "Failure")})
+    public ResponseEntity<?> save(@RequestBody RideRequest rideRequest) {
+        rideRequest.setDate(new Date());    // default to current datetime
+        rideRequest.setStatus(RideRequestStatus.UNASSIGNED);    // default to unassigned status
+
+        RideRequest result = rideRequestRepository.save(rideRequest);
+
+        // create URI of where the rideRequest was created
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest().path("/{id}")
+                .buildAndExpand(result.getId()).toUri();
+
+        return ResponseEntity.created(location).body(result);
+    }
+
+    /*
+     * PUT /rides/{id}
+     */
+    @RequestMapping(method = RequestMethod.PUT, value = "/{id}")
+    @ApiOperation(value = "save", nickname = "save", notes = "Updates the given ride request")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Success", response = ResponseEntity.class),
+            @ApiResponse(code = 401, message = "Unauthorized"),
+            @ApiResponse(code = 403, message = "Forbidden"),
+            @ApiResponse(code = 500, message = "Failure")})
+    public ResponseEntity<?> save(@PathVariable Long id, @RequestBody RideRequest rideRequest) {
+        RideRequest result = rideRequestRepository.save(rideRequest);
+
+        return ResponseEntity.ok(result);
+    }
 
 
-	/*
-	 * DELETE "/rideRequests/{id} 
-	 * 
-	 * @param id - the id of the rideRequest to delete
-	 * 
-	 * @return HTTP response with status of "no content" if deleted successfully
-	 * 		   else response with status of "not found"
-	 * */
-	@RequestMapping(method = RequestMethod.DELETE, value = "/{id}")
-	public ResponseEntity<?> delete(@PathVariable Long id) {
-		if (rideRequestRepository.findOne(id) == null) {
-			return ResponseEntity.notFound().build();
-		} else {
-			rideRequestRepository.delete(id);
-			return ResponseEntity.noContent().build();
-		}
-	}
-	
+    /*
+     * DELETE /rides/{id}
+     */
+    @RequestMapping(method = RequestMethod.DELETE, value = "/{id}")
+    @ApiOperation(value = "delete", nickname = "delete", notes = "Deletes a ride request")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Success", response = ResponseEntity.class),
+            @ApiResponse(code = 401, message = "Unauthorized"),
+            @ApiResponse(code = 403, message = "Forbidden"),
+            @ApiResponse(code = 500, message = "Failure")})
+    public ResponseEntity<?> delete(@PathVariable Long id) {
+        if (rideRequestRepository.findOne(id) == null) {
+            return ResponseEntity.notFound().build();
+        } else {
+            rideRequestRepository.delete(id);
+            return ResponseEntity.noContent().build();
+        }
+    }
+
 }
