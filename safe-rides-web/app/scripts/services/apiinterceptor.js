1b98de1e69e4d931fa2f0725c07edcd461cfb347
shan916 <shan916@icloud.com>
2017-03-24 19:34:29 -0700

    refined login functionality

--- safe-rides-web/app/scripts/services/apiinterceptor.js
+++ safe-rides-web/app/scripts/services/apiinterceptor.js
@@ -9,21 +9,31 @@
  */
 angular.module('safeRidesWebApp')
     .factory('APIInterceptor', function($injector, $window, $cookies, $q) {
+        var redirect = '/';
+
         return {
             request: function(req) {
                 if ($window.localStorage.token) {
-                    req.headers.Authorization = $window.localStorage.token;
+                    req.headers.Authorization = $window.localStorage.safeRidesToken;
                 } else {
-                    req.headers.Authorization = $cookies.get("Authorization");
+                    req.headers.Authorization = $cookies.get('safeRidesToken');
                 }
                 return req;
             },
             responseError: function(rejection, $window) {
+                var state = $injector.get('$state');
+
+                if (redirect !== state.current.name && state.current.name !== 'login') {
+                    redirect = state.current.name;
+                }
+
                 console.log(rejection);
                 switch (rejection.status) {
                     case 401:
+                        state.go('login', {redirect: redirect});
+                        break;
                     case -1:
-                        $injector.get('$state').go('login');
+                        state.go('login', {redirect: redirect});
                         break;
                     default:
                         return $q.reject(rejection);
