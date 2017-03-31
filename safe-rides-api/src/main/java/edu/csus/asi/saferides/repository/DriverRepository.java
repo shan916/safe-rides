0daeccde26253d5f68061913d498a0a44faeb3a9
Edward <ozerugae@users.noreply.github.com>
2017-03-30 19:47:09 -0700

    Feature/pins on map (#62)
    * Get coordinates from address. Need to make google api code more robust
    
    * Minor text fixes
    
    * Move geocoding code out of model into util. Change util class to be @Service
    
    * Display pins on maps
    
    * Fix driver status
    
    * Fix intialization
    
    * Display more pins. make pins clickable
    
    * Add protected constructor to Authority class
    
    * Add driver location to database
    
    * Uncomment geocoding
    
    * Display driver location and details on click
    
    * Minor Changes
    
    Issue still present: UNASSIGNED markers appearing after ASSIGNED filter
    
    * Strict filter match
    
    * Add last names. Display name on pin hover
    
    * Add legend / key for map pins
    
    * Create service: geocodingservice. Make util a static util class

--- safe-rides-api/src/main/java/edu/csus/asi/saferides/repository/DriverRepository.java
+++ safe-rides-api/src/main/java/edu/csus/asi/saferides/repository/DriverRepository.java
@@ -1,8 +1,10 @@
 package edu.csus.asi.saferides.repository;
 
-import java.util.List;
-import org.springframework.data.repository.CrudRepository;
 import edu.csus.asi.saferides.model.Driver;
+import edu.csus.asi.saferides.security.model.User;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
 
 /*
  * @author Zeeshan Khaliq
@@ -14,16 +16,17 @@ import edu.csus.asi.saferides.model.Driver;
  * */
 public interface DriverRepository extends CrudRepository<Driver, Long> {
 
-	/*
-	 * Other query methods can be defined by simply declaring the method signature.
-	 * Spring Data JPA will automagically create an implementation on the fly.
-	 * */
-	List<Driver> findByActive(Boolean active);
-	
-	List<Driver> findByActiveTrueOrderByModifiedDateDesc();
-	
-	List<Driver> findByActiveFalseOrderByModifiedDateDesc();
-	
-	List<Driver> findAllByOrderByModifiedDateDesc();
-	
+    /*
+     * Other query methods can be defined by simply declaring the method signature.
+     * Spring Data JPA will automagically create an implementation on the fly.
+     * */
+    List<Driver> findByActive(Boolean active);
+
+    List<Driver> findByActiveTrueOrderByModifiedDateDesc();
+
+    List<Driver> findByActiveFalseOrderByModifiedDateDesc();
+
+    List<Driver> findAllByOrderByModifiedDateDesc();
+
+    Driver findByUser(User user);
 }
