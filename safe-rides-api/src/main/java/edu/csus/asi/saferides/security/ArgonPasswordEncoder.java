98c0bd4c1964d5cffb6e6d707b19dea107234681
Edward Ozeruga <ed@eoz.io>
2017-04-18 15:37:56 -0700

    ArgonPasswordEncoder.java

--- safe-rides-api/src/main/java/edu/csus/asi/saferides/security/ArgonPasswordEncoder.java
+++ safe-rides-api/src/main/java/edu/csus/asi/saferides/security/ArgonPasswordEncoder.java
@@ -4,13 +4,14 @@ import de.mkammerer.argon2.Argon2;
 import de.mkammerer.argon2.Argon2Factory;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+/**
+ * This class provides a wrapper to allow salting/hashing as well as verifying
+ * passwords against previously salted/hashed passwords
+ * https://github.com/phxql/argon2-jvm
+ */
 public class ArgonPasswordEncoder implements PasswordEncoder {
-	/* This class provides a wrapper to allow salting/hashing as well as verifying 
-	 * passwords against previously salted/hashed passwords
-	 * https://github.com/phxql/argon2-jvm
-	 */
-	
-	// static variables to define size of salt and hash
+
+    // static variables to define size of salt and hash
     private static final int SALTLEN = 32;
     private static final int HASHLEN = 64;
 
@@ -18,7 +19,6 @@ public class ArgonPasswordEncoder implements PasswordEncoder {
     public String encode(CharSequence charSequence) {
         Argon2 argon2 = Argon2Factory.create(Argon2Factory.Argon2Types.ARGON2id, SALTLEN, HASHLEN);
         // Hash password using default values
-        String hash = argon2.hash(2, 65536, 1, charSequence.toString());
         /**
          * String hash(int iterations, int memory, int parallelism, char[] password);
          * Hashes a password.
@@ -29,10 +29,17 @@ public class ArgonPasswordEncoder implements PasswordEncoder {
          * @param charset     Charset of the password
          * @return Hashed password.
          */
+        String hash = argon2.hash(2, 65536, 1, charSequence.toString());
         return hash;
     }
 
-    // matches method allows checking a password against a previously hashed password
+    /**
+     * Matches method allows checking a password against a previously hashed password
+     *
+     * @param charSequence the password from input
+     * @param s            the password from storage
+     * @return boolean if the two parameters match
+     */
     @Override
     public boolean matches(CharSequence charSequence, String s) {
         Argon2 argon2 = Argon2Factory.create(Argon2Factory.Argon2Types.ARGON2id);
