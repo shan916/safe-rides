afbf69642360f71a2a723132cc7182b4cc715312
Justin Mendiguarin <justin.mendiguarin@gmail.com>
2017-04-22 22:34:03 -0700

    documented vehicle class on api

--- safe-rides-api/src/main/java/edu/csus/asi/saferides/model/Vehicle.java
+++ safe-rides-api/src/main/java/edu/csus/asi/saferides/model/Vehicle.java
@@ -32,22 +32,40 @@ public class Vehicle {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
-	
+
+	/**
+	 * Vehicle's make
+	 */
 	@Column(nullable = false)
 	private String make;
-	
+
+	/**
+	 * Vehicle's model
+	 */
 	@Column(nullable = false)
 	private String model;
-	
+
+	/**
+	 * Vehicle's year
+	 */
 	@Column(nullable = false, length = 4)
 	private String year;
-	
+
+	/**
+	 * Vehicle's license plate
+	 */
 	@Column(nullable = false)
 	private String licensePlate;
-	
+
+	/**
+	 * Vehicle's color
+	 */
 	@Column(nullable = false)
 	private String color;
-	
+
+	/**
+	 * Vehicle's seats
+	 */
 	@Column(nullable = false)
 	@Min(2)
 	private Integer seats;
@@ -80,71 +98,156 @@ public class Vehicle {
 		this.color = color;
 		this.seats = seats;
 	}
-	
+
+	/**
+	 * Get driver
+	 *
+	 * @return driver
+	 */
 	public Driver getDriver() {
 		return driver;
 	}
 
+	/**
+	 * Set driver
+	 *
+	 * @param driver
+	 */
 	public void setDriver(Driver driver) {
 		this.driver = driver;
 	}
 
+	/**
+	 * Get vehicle's ID
+	 *
+	 * @return id
+	 */
 	public Long getVehicleId() {
 		return id;
 	}
 
+	/**
+	 * Set vehicle's ID
+	 *
+	 * @param vehicleId
+	 */
 	public void setVehicleId(Long vehicleId) {
 		this.id = vehicleId;
 	}
 
+	/**
+	 * Get vehicle's make
+	 *
+	 * @return make
+	 */
 	public String getMake() {
 		return make;
 	}
 
+	/**
+	 * Set vehicle's make
+	 *
+	 * @param make
+	 */
 	public void setMake(String make) {
 		this.make = make;
 	}
 
+	/**
+	 * Get vehicle's model
+	 *
+	 * @return model
+	 */
 	public String getModel() {
 		return model;
 	}
 
+	/**
+	 * Set vehicle's model
+	 *
+	 * @param model
+	 */
 	public void setModel(String model) {
 		this.model = model;
 	}
 
+	/**
+	 * Get vehicle's year
+	 *
+	 * @return
+	 */
 	public String getYear() {
 		return year;
 	}
 
+	/**
+	 * Set vehicle's year
+	 *
+	 * @param year
+	 */
 	public void setYear(String year) {
 		this.year = year;
 	}
 
+	/**
+	 * Get vehicle's license plate
+	 *
+	 * @return licensePlate
+	 */
 	public String getLicensePlate() {
 		return licensePlate;
 	}
 
+	/**
+	 * Set vehicle's license plate
+	 *
+	 * @param licensePlate
+	 */
 	public void setLicensePlate(String licensePlate) {
 		this.licensePlate = licensePlate;
 	}
 
+	/**
+	 * Get vehicle's color
+	 *
+	 * @return
+	 */
 	public String getColor() {
 		return color;
 	}
 
+	/**
+	 * Set vehicle's color
+	 *
+	 * @param color
+	 */
 	public void setColor(String color) {
 		this.color = color;
 	}
 
+	/**
+	 * Get vehicle's number of seats
+	 *
+	 * @return
+	 */
 	public Integer getSeats() {
 		return seats;
 	}
 
+	/**
+	 * Set vehicle's number of seats
+	 *
+	 * @param seats
+	 */
 	public void setSeats(Integer seats) {
 		this.seats = seats;
 	}
 
+	/**
+	 * Gets a string object representing the values of the Vehicle Object
+	 *
+	 * @return Vehicle string object
+	 */
 	@Override
 	public String toString() {
 		return "Vehicle [driver=" + driver + ", id=" + id + ", make=" + make + ", model=" + model + ", year=" + year
