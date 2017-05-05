22e74da772899689dfce60287235da2fd582cca9
nitronarcosis <nitro.narcosis.biker@gmail.com>
2017-05-04 23:48:35 -0700

    created mapper class

---
+++ safe-rides-api/src/main/java/edu/csus/asi/saferides/mapper/DriverUserMapper.java
@@ -0,0 +1,28 @@
+package edu.csus.asi.saferides.mapper;
+
+import edu.csus.asi.saferides.model.Driver;
+import edu.csus.asi.saferides.model.dto.DriverCreationDto;
+import ma.glasnost.orika.MapperFactory;
+import ma.glasnost.orika.impl.ConfigurableMapper;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DriverUserMapper extends ConfigurableMapper {
+
+	@Override
+	protected void configure(MapperFactory factory) {
+	    factory.classMap(Driver.class, DriverCreationDto.class)
+	        .field("requestorFirstName", "requestorFirstName")
+	    	.field("csusId","csusId")
+		.field("driverFirstName", "driverFirstName")
+		.field("driverLastName", "driverLastName")
+		.field("phoneNumber", "phoneNumber")
+		.field("dlState", "dlState")
+	    .field("dlNumber", "dlNumber")
+	    .field("insuranceChecked", "insuranceChecked")
+	    .field("insuranceCompany", "insuranceCompany")
+	    .field("active", "active")
+	    .byDefault()
+        .register();
+	    }
+}
