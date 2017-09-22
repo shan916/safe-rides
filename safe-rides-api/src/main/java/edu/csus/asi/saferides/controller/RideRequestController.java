a79ec81af8d098a85df31e06a266835ccdbb30fd
Edward Ozeruga <ed@eoz.io>
2017-09-21 19:40:29 -0700

    http -> https

--- safe-rides-api/src/main/java/edu/csus/asi/saferides/controller/RideRequestController.java
+++ safe-rides-api/src/main/java/edu/csus/asi/saferides/controller/RideRequestController.java
@@ -41,7 +41,7 @@ import java.util.List;
  * Rest API controller for the RideRequest resource
  */
 @RestController
-@CrossOrigin(origins = {"http://localhost:9000", "http://codeteam6.io"})
+@CrossOrigin(origins = {"http://localhost:9000", "https://codeteam6.io"})
 @RequestMapping("/rides")
 @PreAuthorize("hasRole('COORDINATOR')")
 public class RideRequestController {
